281a03308d1c218e2d380acb8828662028beb508
muhammadrezasyachrani <rezasyahrani1@gmail.com>
2019-11-10 22:21:30 +0700

    Hitung volume

--- code_program/sensor.ino
+++ code_program/sensor.ino
@@ -1,27 +1,74 @@
-// defines pins numbers
-const int trigPin = 9;
-const int echoPin = 10;
-// defines variables
-long duration;
-int distance;
+int trigPin1 = 6;
+int echoPin1 = 7;
+int trigPin2 = 4;
+int echoPin2 = 5;
+int trigPin3 = 2;
+int echoPin3 = 3;
+double jarakAtas = 30.00;
+double jarakSamping = 30.00;
+double jarakDepan = 30.00;
+
+long duration1, duration2, duration3;
+double jarak1, jarak2, jarak3, tinggi, lebar, panjang, volume;
 void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-Serial.begin(9600); // Starts the serial communication
+  // put your setup code here, to run once:
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(trigPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+
+Serial.begin(9600);
 }
+
 void loop() {
-// Clears the trigPin
-digitalWrite(trigPin, LOW);
+  // put your main code here, to run repeatedly:
+digitalWrite(trigPin1, LOW);
 delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
+digitalWrite(trigPin1, HIGH);
 delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
+digitalWrite(trigPin1, LOW);
+
+duration1 = pulseIn(echoPin1, HIGH);
+jarak1 = duration1*0.034/2;
+tinggi = jarakAtas - jarak1;
+delay (1000);
+
+Serial.print("Tinggi: ");
+Serial.println(tinggi);
+
+digitalWrite(trigPin2, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin2, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin2, LOW);
+
+duration2 = pulseIn(echoPin2, HIGH);
+jarak2 = duration2*0.034/2;
+lebar = jarakSamping - jarak2;
+delay (1000);
+
+Serial.print("Lebar: ");
+Serial.println(lebar);
+
+digitalWrite(trigPin3, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin3, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin3, LOW);
+
+duration3 = pulseIn(echoPin3, HIGH);
+jarak3 = duration3*0.034/2;
+panjang = jarakDepan - jarak3;
+delay (1000);
+
+Serial.print("Panjang: ");
+Serial.println(panjang);
+
+volume = panjang * lebar * tinggi;
+Serial.print("Volume: ");
+Serial.println(volume);
+
+
 }
